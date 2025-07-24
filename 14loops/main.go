a382d3b14aac59c82eec543646c59c199962ce1f
Deeptansh Nagar <diptanshnagar@gmail.com>
2025-07-24 18:06:56 +0530

    loops

---
+++ 14loops/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Welcome to loops in golang")
+
+	days := []string{"Sunday", "Tuesday", "Wednesday", "Friday", "Saturday"}
+
+	fmt.Println(days)
+}
