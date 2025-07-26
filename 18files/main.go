88423899790bc603a23e0ebfeb0648840b8fd875
Deeptansh Nagar <diptanshnagar@gmail.com>
2025-07-26 11:00:13 +0530

    file handling

---
+++ 18files/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+)
+
+// Writing the file
+func main() {
+	fmt.Println("Welcome to files in golang")
+	content := "This needs to go in a file - LearnCodeOnline.in"
+
+	file, err := os.Create("./mylcogofile.txt")
+	
+	if err != nil {
+		panic(err)
+	}
+
+	length, err  := io.WriteString(file, content)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("length is: ", length)
+	defer file.Close()
+	readFile("./mylcogofile.txt")
+}
+
+// Reading the file 
+func readFile(filename string) {
+	databyte, err := ioutil.ReadFile(filename) // file isnt being read in string format, its being read in byte format.
+	if err != nil {
+		panic(err)
+	}
+
+	// fmt.Println("Text data inside the file is \n", databyte)
+	fmt.Println("Text data inside the file is \n", string(databyte))
+}
+
+func checkNilErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
